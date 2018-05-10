552af8ac8232e6dc8672f196ae2ac5d113624f98
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-10 07:49:54 +0100

    Extract resolvedStatusCode function.

--- src/main/scala/io/sdkman/UrlValidation.scala
+++ src/main/scala/io/sdkman/UrlValidation.scala
@@ -9,6 +9,10 @@ case class Cookie(name: String, value: String)
 
 trait UrlValidation {
   def hasOrphanedUrl(url: String, cookie: Option[Cookie] = None): Boolean =
+    resolvedStatusCode(url, cookie)
+      .fold(e => true, code => Seq(404, 403, 401).contains(code))
+
+  def resolvedStatusCode(url: String, cookie: Option[Cookie] = None): Try[Int] =
     Try {
       val http = Http(url)
         .method("HEAD")
@@ -16,5 +20,5 @@ trait UrlValidation {
       cookie.fold(http)(c => http.cookie(c.name, c.value))
         .asString
         .code
-    }.fold(e => true, code => Seq(404, 403, 401).contains(code))
+    }
 }
