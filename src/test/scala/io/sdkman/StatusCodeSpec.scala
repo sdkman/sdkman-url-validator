ae731d3f083f8128e0cb0e9a3a454df05537842e
Marco Vermeulen <vermeulen.mp@gmail.com>
2019-05-20 20:10:53 +0100

    Ignore broken Oracle URL specs.

--- src/test/scala/io/sdkman/StatusCodeSpec.scala
+++ src/test/scala/io/sdkman/StatusCodeSpec.scala
@@ -13,13 +13,13 @@ class StatusCodeSpec extends WordSpec with Matchers with BeforeAndAfter with Try
       resolvedStatusCode("https://github.com/sbt/sbt/releases/download/v1.1.4/sbt-1.1.4.zip").success.value shouldBe 200
     }
 
-    "report a 200 status code for a valid oracle url" in new TestValidation {
+    "report a 200 status code for a valid oracle url" ignore new TestValidation {
       resolvedStatusCode(
         "http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-x64_bin.tar.gz",
         Some(Cookie("oraclelicense", "accept-securebackup-cookie"))).success.value shouldBe 200
     }
 
-    "report a failure for an invalid oracle url" in new TestValidation {
+    "report a failure for an invalid oracle url" ignore new TestValidation {
       resolvedStatusCode(
         "http://download.oracle.com/otn-pub/java/jdk/10.0.0+1/fb4372174a714e6b8c52526dc134031e/jdk-10.0.0_linux-x64_bin.tar.gz",
         Some(Cookie("oraclelicense", "accept-securebackup-cookie"))).isFailure shouldBe true
